7c9fddafc6544d6796addcf519d9eafd546f710e
ianpatrani <86625700+ianpatrani@users.noreply.github.com>
2021-10-21 10:14:29 -0300

    Add files via upload

---
+++ PersonaPrueba/Persona.js
@@ -0,0 +1,39 @@
+class Persona {
+    static contadorObjetosPersona = 0;
+
+    constructor(nombre, apellido, edad) {
+        this._idPersona = ++Persona.contadorPersona;
+        this._nombre = nombre;
+        this._apellido = apellido;
+        this._edad = edad;
+    }
+    get idPersona() {
+        return this._idPersona;
+    }
+    get edad() {
+        return this._edad;
+    }
+    set edad(edad) {
+        this._edad = edad;
+    }
+    get nombre() {
+        return this._nombre;
+    }
+    set nombre(nombre) {
+        this._nombre = nombre;
+    }
+    get apellido() {
+        return this._apellido;
+    }
+    set apellido(apellido) {
+        this._apellido = apellido;
+    }
+    toString() {
+        return
+               `${this._idPersona}
+                ${this._nombre}
+                ${this._apellido}
+                ${this._edad}`;
+    }
+}
+
